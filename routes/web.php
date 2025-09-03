e3804a2b92f394efe03a373332c487b87eb6772b
SatyaAdil <adilfaishal11@gmail.com>
2025-09-04 02:39:09 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -69,3 +69,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// update
